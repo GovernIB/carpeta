7053b37a51bdd001fa9e8f1c5617c6f14bcb0fd7
jpernia <jpernia@fundaciobit.org>
2019-06-10 14:18:16 +0200

    Afegir javascripts nous

--- carpeta-front/src/main/webapp/static/js/carpeta.js
+++ carpeta-front/src/main/webapp/static/js/carpeta.js
@@ -1,75 +1,52 @@
-/** Recalcula els menús segons ample de finestra **/
+/** Recalcula los menús según ancho de ventana **/
 $( window ).resize(function() {
   var ventana_ancho = $(window).width();
+  if(ventana_ancho>=810){
+	$("#navigation").addClass("noVisible");
+	$("#opcionesLargo").removeClass("noVisible");
+	$("#opcionesMenu").removeClass("noVisible");
+  }
+  if(ventana_ancho<810 & ventana_ancho>=640){
+	$("#navigation").addClass("noVisible");
+	$("#opcionesLargo").removeClass("noVisible");
+	$("#opcionesMenu").removeClass("noVisible");
+	$("#menuUsuario").removeClass("col-9");
+	$("#menuUsuario").addClass("col-8");
+  }
   if(ventana_ancho<641){
-	$("#navigation").removeClass("no-visible");
-	$("#opcionsLlarg").addClass("no-visible");
-	$("#imc-menu").addClass("no-visible");
-  }else{
-	$("#navigation").addClass("no-visible");
-	$("#opcionsLlarg").removeClass("no-visible");
-	$("#imc-menu").removeClass("no-visible");
+	$("#navigation").removeClass("noVisible");
+	$("#opcionesLargo").addClass("noVisible");
+	$("#opcionesMenu").addClass("noVisible");
+
   }
 });
 
-/** Calcula el tamany del height del menú 
-$(document).ready(function($){
-	altMenu = $('#imc-cap').outerHeight();
-	alert(altMenu);
-	$('#imc-menu').css({'padding-top': altMenu});
-});**/
-
-/** Obre els idiomes disponibles del menú **/
-$(document).ready(function() {
-$("#imc-bt-idioma").hover(function(){
-  $("#idioma").css("display", "grid");
-  }, function(){
-  $("#idioma").css("display", "none");
-});
-
-});
-
-/** Mostra els menús segons ample de finestra **/
+/** Muestra los menús según ancho de ventana **/
 $(document).ready(function($){
   var ventana_ancho = $(window).width();
-  if(ventana_ancho<780){
-	$("#id_ca").text("ca");
-	$("#id_es").text("es");
+  if(ventana_ancho<810 & ventana_ancho>=640){
+	$("#opcionesLargo").removeClass("col-5");
+	$("#opcionesLargo").addClass("col-3");
+	$("#idCa").text("ca");
+	$("#idEs").text("es");
   }
   if(ventana_ancho<641){
-	$("#navigation").removeClass("no-visible");
-	$("#opcionsLlarg").addClass("no-visible");
-	$("#imc-menu").addClass("no-visible");
+	$("#navigation").removeClass("noVisible");
+	$("#opcionesLargo").addClass("noVisible");
+	$("#opcionesMenu").addClass("noVisible");
+	$("#menuUsuario").removeClass("col-8");
+	$("#menuUsuario").addClass("col-10");
   }
 });
 
-
-/** Obre el quadre d'ajuda **/
-function obrirAjuda(){
-	$("#imc-suport").addClass("imc--on");
-    $("#imc-suport-capsa").addClass("imc--on");
+/** Abre el cuadro de ayuda **/
+function abrirAyuda(){
+	$("#soporte").addClass("on");
+    $("#cajaSoporte").addClass("on");
 }
 
-/** Tanca el quadre d'ajuda **/
-function tancarAjuda(){
-	$("#imc-suport").removeClass("imc--on");
-    $("#imc-suport-capsa").removeClass("imc--on");
-}
-
-/** Per sortir de la Carpeta **/
-function sortirCarpeta(){
-    document.getElementById('logout').submit();
-}
-
-
-/** Controla el menú per a mòbils **/
-function desplegarMenu(){
-	var obert = $("#menu-mobil").hasClass("obert");
-	if(!obert){
-		$("#menu-mobil").addClass("obert");
-		$("#menu-mobil").removeClass("invisible");
-	}else{
-		$("#menu-mobil").removeClass("obert");
-		$("#menu-mobil").addClass("invisible");
-	}
+/** Cierra el cuadro de ayuda **/
+function cerrarAyuda(){
+	$("#soporte").removeClass("on");
+    $("#cajaSoporte").removeClass("on");
 }
