c9a8d7db8a032797d037b508fb04883c1875ed86
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2020-05-29 07:57:38 +0200

    Definició Model de Dades #50   corregido un error, eliminado unique de nif de acceso.

--- carpeta-persistence/src/main/java/es/caib/carpeta/persistence/Estadistica.java
+++ carpeta-persistence/src/main/java/es/caib/carpeta/persistence/Estadistica.java
@@ -26,7 +26,7 @@ import java.util.Objects;
    indexes = {
       @Index(name = "CAR_ESTADISTICA_PK_I", columnList = "ID"),
       @Index(name = "CAR_ESTAD_ACCESO_FK_I", columnList = "ACCESO"),
-      @Index(name = "CAR_ESTAD_ENTIDAD_FK_I", columnList = "ENTIDAD"),
+      @Index(name = "CAR_ESTAD_ENTIDAD_FK_I", columnList = "ENTIDAD")
    }
 )
 
