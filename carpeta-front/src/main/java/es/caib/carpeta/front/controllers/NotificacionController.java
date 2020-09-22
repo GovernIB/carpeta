7b7c0769e0a37f1a4d6449b4fe8747661e4dc184
u139636 <scalafat@dgtic.caib.es>
2020-09-22 12:33:12 +0200

    Nova pantalla de comunicacions/notificacions de SISTRA1 #115

--- carpeta-front/src/main/java/es/caib/carpeta/front/controllers/NotificacionController.java
+++ carpeta-front/src/main/java/es/caib/carpeta/front/controllers/NotificacionController.java
@@ -1,23 +1,24 @@
 package es.caib.carpeta.front.controllers;
 
 import es.caib.carpeta.core.service.Sistra1Service;
+import es.caib.carpeta.front.config.UsuarioAutenticado;
+import es.caib.carpeta.utils.CarpetaConstantes;
 import es.caib.zonaper.ws.v2.model.elementoexpediente.ElementoExpediente;
+import es.caib.zonaper.ws.v2.model.elementoexpediente.TipoElementoExpediente;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 
 @Controller
@@ -38,6 +39,7 @@ public class NotificacionController {
     public ModelAndView tramites(ModelMap model, Authentication authentication) throws Exception {
 
         ModelAndView mav = new ModelAndView("notificaciones");
+        mav.addObject("autenticacio", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         mav.addObject("zonaperUrl", ZONAPER_URL);
         mav.addObject("notificacionesUrl", NOTIFICACIONES_URL);
         mav.addObject("breadcrumb", Arrays.asList("inicio", "notificaciones"));
@@ -51,19 +53,27 @@ public class NotificacionController {
     @RequestMapping(value = { "/comunicaciones"}, method = RequestMethod.GET)
     public ModelAndView comunicaciones(ModelMap model, Authentication authentication) throws Exception {
 
-        log.debug("COMUNICACIONES antes de llamar");
-        List<ElementoExpediente> comunicaciones = sistra1Service.obtenerElementosExpediente().getElemento();
+        UsuarioAutenticado usuarioAutenticado = (UsuarioAutenticado)authentication.getPrincipal();
+        Locale loc = LocaleContextHolder.getLocale();
+
+        List<TipoElementoExpediente> coms = new ArrayList<TipoElementoExpediente>();
+        coms.add(TipoElementoExpediente.COMUNICACION);
+        coms.add(TipoElementoExpediente.NOTIFICACION);
+//        coms.add(TipoElementoExpediente.ENVIO);
+//        coms.add(TipoElementoExpediente.REGISTRO);
+//        coms.add(TipoElementoExpediente.PREREGISTRO);
+//        coms.add(TipoElementoExpediente.PREENVIO);
+
+        List<ElementoExpediente> comunicaciones = sistra1Service.obtenerElementosExpediente(coms, CarpetaConstantes.ELEMENTO_TODOS, usuarioAutenticado.getUsuarioClave(), loc);
 
         ModelAndView mav = new ModelAndView("comunicaciones");
 
-
-        log.debug("COMUNICACIONES después de llamar");
-
+        mav.addObject("autenticacio", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         mav.addObject("sistraZonaperUrl", SISTRA1_ZONAPER_URL);
         mav.addObject("notificacionesUrl", NOTIFICACIONES_URL);
         mav.addObject("comunicacions", comunicaciones);
-        mav.addObject("breadcrumb", Arrays.asList("inicio", "notificaciones", "notificaciones.otras"));
-        Locale loc = LocaleContextHolder.getLocale();
+        mav.addObject("breadcrumb", Arrays.asList("inicio", "notificacion/list", "notificaciones.otras"));
+
         mav.addObject("title_page", ResourceBundle.getBundle("mensajes", loc).getString("titulo.comunicaciones"));
 
         return mav;
