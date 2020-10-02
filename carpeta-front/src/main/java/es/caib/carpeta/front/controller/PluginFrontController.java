823e7a8c8bcf0d52f320b99d47e8a53cdbe0937e
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2020-10-02 10:01:00 +0200

    Afegir icones per cada plugin #124

--- carpeta-front/src/main/java/es/caib/carpeta/front/controller/PluginFrontController.java
+++ carpeta-front/src/main/java/es/caib/carpeta/front/controller/PluginFrontController.java
@@ -154,8 +154,6 @@ public class PluginFrontController {
             response.setContentLength((int) fi.getData().length);
 
             response.getOutputStream().write(fi.getData());
-        }else{
-
         }
     }
     
