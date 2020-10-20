b1da6e248da259b089daa39ab0a0339c9f990424
jpernia <jpernia@fundaciobit.org>
2020-10-20 13:55:06 +0200

    #171

--- react/src/MenuLateral.js
+++ react/src/MenuLateral.js
@@ -1,64 +1,16 @@
 import React, {Component} from 'react';
 import { withTranslation } from 'react-i18next';
+import LogoLateral from './LogoLateral';
+import EnllasosLateral from './EnllasosLateral';
 
 class MenuLateral extends Component {
 
-    constructor(){
-        super();
-        this.state = { infologolateral: [] }
-    }
-
-    componentWillMount() {
-        fetch(window.location.href + "webui/infologolateral")
-            .then((response) => {
-                return response.json()
-            })
-            .then((infologolateral) => {
-                this.setState({ infologolateral: infologolateral })
-            })
-    }
-
     render() {
-
-        const {t} = this.props;
-
-        let infologolateral;
-
-        if(!this.state.infologolateral.length){
-            infologolateral = "";
-        } else{
-            infologolateral = this.state.infologolateral.map((s, i) => (
-                <a href={s.url} className="imc--goib" title={s.label}>
-                    <img src={s.urllogo} title={s.label} alt={s.label} />
-                    <span>{s.label}</span>
-                </a>
-            ))
-        }
-
 
         return (
             <div className="imc-logo">
-                {infologolateral}
-                <ul>
-                    <li>
-                        <a href="http://www.caib.es/pidip2front/jsp/ca/noticies" className="imc-logo-ico imc--informat"
-                           title={t('menuLateralNoticies')}>
-                            <span>{t('menuLateralNoticies')}</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://www.caib.es/govern/administracio.do?lang=ca"
-                           className="imc-logo-ico imc--administracio" title={t('menuLateralAdministracio')}>
-                            <span>{t('menuLateralAdministracio')}</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://www.illesbalears.travel/?lang=ca" className="imc-logo-ico imc--illes"
-                           title={t('menuLateralIlles')}>
-                            <span>{t('menuLateralIlles')}</span>
-                        </a>
-                    </li>
-                </ul>
+                <LogoLateral />
+                <EnllasosLateral />
             </div>
         );
     }
