64e9ddddb3fdd84b657ad8d0a45192991448b2f8
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2020-10-27 14:37:14 +0100

    Text informatiu de l'entitat del peu de pàgina del Front #169.

--- react/src/DadesEntitat.js
+++ react/src/DadesEntitat.js
@@ -1,43 +1,38 @@
 import {Component} from 'react';
 import {withTranslation} from 'react-i18next';
+import React from "./main";
 
 class DadesEntitat extends Component {
 
     constructor(){
         super();
-        this.state = {
-            textinformatiuentitat: []
 
-        }
     }
 
-    componentWillMount() {
-        fetch(window.location.href + "webui/textinformatiuentitat")
-            .then((response) => {
-                return response.json()
-            })
-            .then((textinformatiuentitat) => {
-                this.setState({ textinformatiuentitat: textinformatiuentitat })
-            });
-    }
+
 
     render() {
 
-        const {t} = this.props;
+        var urlBase = window.location.href;
+        var url = urlBase + "/textinformatiuentitat";
 
-        let laterallink;
-
-        if(!this.state.textinformatiuentitat.length){
-            laterallink = "";
-        } else{
-            laterallink = this.state.textinformatiuentitat;
-        }
-
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onload = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    document.getElementById("substituir").innerHTML = this.responseText;
+                } else {
+                    window.location.href = window.location.href;
+                }
+            }
+        };
+        xhr.send(data);
 
         return (
-                {laterallink}
+            <div id="substituir"></div>
         );
     }
 }
 
-export default withTranslation()(EnllasosLateral);
+export default withTranslation()(DadesEntitat);
