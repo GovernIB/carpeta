55e360c1b450bd7bf508aa1a1299932afb5ba33e
mgonzalez-fundaciobit <mgonzalez@fundaciobit.org>
2020-11-03 09:27:31 +0100

    faltava afegir @Local

--- carpeta-logic/src/main/java/es/caib/carpeta/logic/LogCarpetaLogicaLocal.java
+++ carpeta-logic/src/main/java/es/caib/carpeta/logic/LogCarpetaLogicaLocal.java
@@ -3,12 +3,15 @@ package es.caib.carpeta.logic;
 import es.caib.carpeta.ejb.LogCarpetaLocal;
 import org.fundaciobit.genapp.common.i18n.I18NException;
 
+import javax.ejb.Local;
+
 /**
  * Created by Fundació BIT.
  *
  * @author mgonzalez
  * Date: 13/10/2020
  */
+@Local
 public interface LogCarpetaLogicaLocal extends LogCarpetaLocal {
 
     public static final String JNDI_NAME = "java:app/carpeta-logic/LogCarpetaLogicaEJB!es.caib.carpeta.logic.LogCarpetaLogicaLocal";
