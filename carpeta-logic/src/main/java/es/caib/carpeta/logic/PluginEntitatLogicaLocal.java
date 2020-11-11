da38efc0e4ef4200177cd1d7e4c248922a6c7415
Juan Antonio Garcia <jagarcia@fundaciobit.org>
2020-11-11 09:09:17 +0100

    Validació en el formulari de creació d'un avis #189

---
+++ carpeta-logic/src/main/java/es/caib/carpeta/logic/PluginEntitatLogicaLocal.java
@@ -0,0 +1,26 @@
+package es.caib.carpeta.logic;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.validation.constraints.NotNull;
+
+import org.fundaciobit.genapp.common.i18n.I18NException;
+
+import es.caib.carpeta.ejb.PluginEntitatLocal;
+import es.caib.carpeta.jpa.PluginEntitatJPA;
+
+/**
+ * 
+ * @author jagarcia
+ *
+ */
+@Local
+public interface PluginEntitatLogicaLocal extends PluginEntitatLocal  {
+	
+	public static final String JNDI_NAME = "java:app/carpeta-logic/PluginEntitatLogicaEJB!es.caib.carpeta.logic.PluginEntitatLogicaLocal";
+
+	public List<PluginEntitatJPA> findAllByEntitatId(@NotNull long codiEntitat) throws I18NException;
+
+}
+
