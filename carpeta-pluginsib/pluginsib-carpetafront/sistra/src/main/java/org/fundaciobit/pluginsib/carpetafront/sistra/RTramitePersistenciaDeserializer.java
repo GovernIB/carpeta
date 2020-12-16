8ae33ea2ff3ac2617d0500037296c416003fd1bf
Juan Antonio Garcia <jagarcia@fundaciobit.org>
2020-12-16 15:07:02 +0100

    Refactorització de tràmits telemàtics mostrats #231

--- carpeta-pluginsib/pluginsib-carpetafront/sistra/src/main/java/org/fundaciobit/pluginsib/carpetafront/sistra/RTramitePersistenciaDeserializer.java
+++ carpeta-pluginsib/pluginsib-carpetafront/sistra/src/main/java/org/fundaciobit/pluginsib/carpetafront/sistra/RTramitePersistenciaDeserializer.java
@@ -7,46 +7,58 @@ import javax.json.stream.JsonParser;
 import es.caib.sistramit.rest.api.externa.v1.RTramitePersistencia;
 
 import java.lang.reflect.Type;
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
 
 import static javax.json.stream.JsonParser.Event;
 
 public class RTramitePersistenciaDeserializer implements JsonbDeserializer<RTramitePersistencia> {
+	
+	private static final Logger logger = Logger.getLogger(RTramitePersistenciaDeserializer.class.getName());
 
     @Override
     public RTramitePersistencia deserialize(JsonParser parser, DeserializationContext ctx, Type rtType) {
+    	
         RTramitePersistencia tramite = new RTramitePersistencia();
 
-        while (parser.hasNext()) {
-            Event event = parser.next();
-            if (event == Event.KEY_NAME) {
-                String keyName = parser.getString();
-                parser.next();
-               switch (keyName) {
-                    case "idTramite":
-                        tramite.setIdTramite(parser.getString());
-                    break;
-                    case "descripcionTramite":
-                        tramite.setDescripcionTramite(parser.getString());
-                    break;
-                    case "idioma":
-                        tramite.setIdioma(parser.getString());
-                    break;
-                    case "idSesionTramitacion":
-                        tramite.setIdSesionTramitacion(parser.getString());
-                    break;
-                    case "versionTramite":
-                        tramite.setVersionTramite(parser.getInt());
-                    break;
-                    case "fechaInicio":
-                        tramite.setFechaInicio(new Date(parser.getLong()));
-                    break;
-                    case "fechaUltimoAcceso":
-                        tramite.setFechaUltimoAcceso(new Date(parser.getLong()));
-                    break;
+        try {
+        	
+        	while (parser.hasNext()) {
+                Event event = parser.next();
+                if (event == Event.KEY_NAME) {
+                    String keyName = parser.getString();
+                    parser.next();
+                   switch (keyName) {
+                        case "idTramite":
+                            tramite.setIdTramite(parser.getString());
+                        break;
+                        case "descripcionTramite":
+                            tramite.setDescripcionTramite(parser.getString());
+                        break;
+                        case "idioma":
+                            tramite.setIdioma(parser.getString());
+                        break;
+                        case "idSesionTramitacion":
+                            tramite.setIdSesionTramitacion(parser.getString());
+                        break;
+                        case "versionTramite":
+                            tramite.setVersionTramite(parser.getInt());
+                        break;
+                        case "fechaInicio":
+                            tramite.setFechaInicio(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(parser.getString()));
+                        break;
+                        case "fechaUltimoAcceso":                    	
+                            tramite.setFechaUltimoAcceso(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(parser.getString()));
+                        break;
+                    }
                 }
             }
+        	
+        }catch (ParseException e) {
+        	logger.info("ERROR RTramitePersistencia: " + e.getMessage()); 
         }
+        
         return tramite;
     }
 }
