9fedefbe686f815a8e27a73af6a76a0e5d808ba3
anadal-fundaciobit <anadal@fundaciobit.org>
2021-02-08 11:07:08 +0100

    Permetre Seccions en Plugins i enllaços de tipus PseudoPlugin #335

---
+++ scripts/bbdd/oracle/update_schema_from_1.1.7_to_1.1.8.sql
@@ -0,0 +1,38 @@
+﻿
+
+-- 08/02/2021 Permetre Seccions en Plugins i enllaços de tipus PseudoPlugin #335
+
+create sequence car_seccio_seq start with 1000 increment by  1;
+
+CREATE TABLE car_seccio
+(
+   seccioid NUMBER(19,0) NOT NULL DEFAULT car_seccio_seq.nextval, 
+   nomid NUMBER(19,0) NOT NULL, 
+   descripcioid NUMBER(19,0) NOT NULL, 
+   activa INTEGER NOT NULL DEFAULT 1, 
+   iconaid NUMBER(19,0) NOT NULL, 
+   secciopareid NUMBER(19,0)
+);
+
+ALTER TABLE car_seccio CONSTRAINT car_seccio_pk PRIMARY KEY (seccioid);
+ALTER TABLE car_seccio CONSTRAINT car_seccio_traduccio_nom_fk FOREIGN KEY (nomid) REFERENCES car_traduccio (traduccioid);
+ALTER TABLE car_seccio CONSTRAINT car_seccio_traduccio_des_fk FOREIGN KEY (descripcioid) REFERENCES car_traduccio (traduccioid);
+ALTER TABLE car_seccio CONSTRAINT car_seccio_fitxer_icon_fk FOREIGN KEY (iconaid) REFERENCES car_fitxer (fitxerid);
+
+create index car_seccio_pk_i on car_seccio (seccioid);
+
+create index car_seccio_nomid_fk_i on car_seccio (nomid);
+
+create index car_seccio_descripcioid_fk_i on car_seccio (descripcioid);
+
+create index car_seccio_iconaid_fk_i on car_seccio (iconaid);
+
+ALTER TABLE car_plugin  ADD COLUMN seccioid NUMBER(19,0);
+ALTER TABLE car_plugin  ADD CONSTRAINT car_plugin_seccio_sec_fk FOREIGN KEY (seccioid) REFERENCES car_seccio (seccioid) ;
+create index car_plugin_seccioid_fk_i on car_plugin (seccioid);
+
+
+ALTER TABLE car_enllaz  ADD COLUMN seccioid NUMBER(19,0);
+ALTER TABLE car_enllaz  ADD CONSTRAINT car_enllaz_seccio_sec_fk FOREIGN KEY (seccioid) REFERENCES car_seccio (seccioid) ;
+create index car_enllaz_seccioid_fk_i on car_enllaz (seccioid);
+
