9ca12b4cd6187a038ac8da8a5ad40fa68126e40d
anadal-fundaciobit <anadal@fundaciobit.org>
2021-02-19 09:02:01 +0100

    Eliminar mètodes new* i fer un ús pur de instanciació React #359

--- carpeta-front/react/webpack.config.js
+++ carpeta-front/react/webpack.config.js
@@ -3,32 +3,32 @@ const path = require("path");
 const config = {
 	entry: "./src/main.js",
 	output: {
-	  path: path.resolve(__dirname, "dist"),
-	  filename: "reactjs_main.js"
-},
+		path: path.resolve(__dirname, "dist"),
+		filename: "reactjs_main.js"
+	},
 
-module: {
-  
-  rules: [
-  {
-      test:/\.(js|jsx)$/,
-      loaders: "babel-loader",
-      query: {
+	module: {
 
-		  "presets": [
-				//"env",
-				"@babel/preset-react"
-				//"stage-2",
-				//"@babel/react"
-			  ]
-			  //"plugins": [["flow-runtime"], "transform-decorators-legacy"]
-		  
-		  
-      }
-  },
-  { test: /\.(png|jpg|)$/, loader: 'url-loader?limit=240' }
-]
-}
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				loaders: "babel-loader",
+				query: {
+
+					"presets": [
+						//"env",
+						"@babel/preset-react"
+						//"stage-2",
+						//"@babel/react"
+					]
+					//"plugins": [["flow-runtime"], "transform-decorators-legacy"]
+
+
+				}
+			},
+			{ test: /\.(png|jpg|)$/, loader: 'url-loader?limit=240' }
+		]
+	}
 };
 
 module.exports = config;
