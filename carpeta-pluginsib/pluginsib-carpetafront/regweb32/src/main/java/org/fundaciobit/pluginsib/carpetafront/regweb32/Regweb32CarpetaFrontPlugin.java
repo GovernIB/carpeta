ddbf364d571c6703eb9ef7544f1bf0afafaefaee
Juan Antonio Garcia <jagarcia@fundaciobit.org>
2021-05-19 14:30:47 +0200

    Afegir filtres de cerca al plugin de Regweb32 #483

--- carpeta-pluginsib/pluginsib-carpetafront/regweb32/src/main/java/org/fundaciobit/pluginsib/carpetafront/regweb32/Regweb32CarpetaFrontPlugin.java
+++ carpeta-pluginsib/pluginsib-carpetafront/regweb32/src/main/java/org/fundaciobit/pluginsib/carpetafront/regweb32/Regweb32CarpetaFrontPlugin.java
@@ -24,7 +24,12 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
-
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.sql.Timestamp;
+import java.text.DateFormat;
 
 /**
  * @author anadal
@@ -162,15 +167,8 @@ public class Regweb32CarpetaFrontPlugin extends RegwebDetallComponent {
                 
             } else if (query.startsWith(LLISTAT_REGISTRES_PAGE)) {
 
-                String pageNumber = request.getParameter("pageNumber");
-                if (pageNumber == null) {
-                    pageNumber = "1";
-                }
-
-                log.info("PAGE NUMBER " + pageNumber);
-
                 llistatDeRegistres(absolutePluginRequestPath, relativePluginRequestPath, query, request, response,
-                        userData, administrationEncriptedID, Integer.parseInt(pageNumber) - 1, locale, isGet);
+                        userData, administrationEncriptedID, locale, isGet);
 
             }  else {
 
@@ -273,20 +271,69 @@ public class Regweb32CarpetaFrontPlugin extends RegwebDetallComponent {
 
     protected static final String LLISTAT_REGISTRES_PAGE = "llistatRegistres32";
 
-
     public void llistatDeRegistres(String absolutePluginRequestPath, String relativePluginRequestPath, String query,
             HttpServletRequest request, HttpServletResponse response, UserData userData,
-            String administrationEncriptedID, int pageNumber, Locale locale, boolean isGet) {
+            String administrationEncriptedID, Locale locale, boolean isGet) {
 
         try {
+        	
+        	/* Filtre número de registre */
+        	String formNumero = request.getParameter("numero");
+        	
+        	/* Filtre pàgina */
+        	String pageNumber = request.getParameter("pageNumber");
+            if (pageNumber == null) {
+                pageNumber = "1";
+            }
+            
+            /* Filtre estat */
+            String formEstat = (request.getParameter("estado") != null) ? request.getParameter("estado") : "0";
+        	
+            /* Filtre dates*/
+        	Date formDataInici;
+            Date formDataFi;
+            String formDataIniciStr = request.getParameter("fechaInicio");
+            String formDataFiStr = request.getParameter("fechaFin");
+            
+            String parametros = "";
+    		Calendar cal = Calendar.getInstance();
+    		SimpleDateFormat SDF = new SimpleDateFormat("dd/MM/yyyy");
+            
+            if(formDataFiStr != null && formDataFiStr != "") {
+            	formDataFi = SDF.parse(formDataFiStr);
+            	parametros += "&fechaFin=" + formDataFiStr;
+            }else {
+            	formDataFi = cal.getTime();
+            }
+            
+            if  (formDataIniciStr != null && formDataIniciStr != ""){
+            	formDataInici = SDF.parse(formDataIniciStr);
+            	parametros +=  "&fechaInicio=" + formDataIniciStr;
+            }else {
+            	/* Inicialitzam darrers 6 mesos */
+            	cal.add(Calendar.MONTH, -6);
+            	formDataInici = cal.getTime();
+            }
+            
+            if(formEstat != null && Integer.parseInt(formEstat)> 0){
+            	parametros +=   "&estado=" + formEstat;
+            }
+            
+            if (isDevelopment()) {
+            	log.info("NUMERO => " + formNumero);
+            	log.info("PAGE NUMBER =>" + pageNumber);
+            	log.info("DATA INICI => " + formDataIniciStr );
+            	log.info("DATA FI => " + formDataFiStr);
+            	log.info("ESTAT => " + formEstat); 
+            }
+        
+            Thread.sleep(5000);
 
             response.setCharacterEncoding("utf-8");
             response.setContentType("text/html");
 
-            
-
             String webpage = getLlistatDeRegistresPage(absolutePluginRequestPath, relativePluginRequestPath, userData,
-                    getEntidad(), pageNumber, locale, isGet);
+                    getEntidad(), formNumero, formDataInici, formDataFi, formEstat, parametros, Integer.parseInt(pageNumber) - 1, locale, isGet);
 
             try {
                 response.getWriter().println(webpage);
@@ -308,13 +355,18 @@ public class Regweb32CarpetaFrontPlugin extends RegwebDetallComponent {
     }
 
     public String getLlistatDeRegistresPage(String absolutePluginRequestPath, String relativePluginRequestPath,
-            UserData userData, String entidad, int pageNumber, Locale locale, boolean isGet) throws Exception {
+            UserData userData, String entidad, String formNumero, Date formDataInici, Date formDataFi, String formEstat, String parametros, int pageNumber, Locale locale, boolean isGet) throws Exception {
 
 
         Map<String, Object> map = new HashMap<String, Object>();
+        
+        map.put("form_numero", formNumero);
+        map.put("form_dataFi", formDataFi);
+        map.put("form_dataInici", formDataInici);
+        map.put("form_estat", formEstat);
 
         ResultadoBusquedaWs result;
-        result = getRegistres(userData.getAdministrationID(), entidad, pageNumber, locale);
+        result = getRegistres(userData.getAdministrationID(), entidad, formNumero, formDataInici, formDataFi, formEstat, pageNumber, locale);
 
         @SuppressWarnings("unchecked")
         List<AsientoWs> registres = (List<AsientoWs>) (List<?>) result.getResults();
@@ -332,10 +384,50 @@ public class Regweb32CarpetaFrontPlugin extends RegwebDetallComponent {
         map.put("lang", locale.getLanguage());
 
         map.put("title", getTitle(locale));
+        
+        String[] registroEstado = { "", getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.1", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.2", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.3", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.4", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.5", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.6", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.7", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.8", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.9", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.10", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.11", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.12", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.13", locale)};
+
+        map.put("registroEstado", registroEstado);
+        
+        String[] registroEstadoSelect = { "", getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.1", locale),
+        		getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.4", locale),
+        		getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.10", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.11", locale)};
+        
+        map.put("registroEstadoSelect", registroEstadoSelect);
+        
+        String[] registroExplicacionEstado = { "", 
+        		getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.1", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.2", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.3", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.4", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.5", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.6", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.7", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.8", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.9", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.10", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.11", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.12", locale),
+                getTraduccio(RESOURCE_BUNDLE_NAME, "registro.estado.explicacion.13", locale)};
+
+        map.put("registroExplicacionEstado", registroExplicacionEstado);
 
         String[] traduccions = {"registro.listado", "registro.descripcion", "registro.numero", "registro.fecha",
-                "registro.extracto", "registro.destinatario", "registro.vacio", "carpeta.acciones",
-                "registro.detalle"};
+                "registro.extracto", "registro.destinatario", "registro.vacio", "carpeta.acciones",  "registro.estado",
+                "registro.detalle", "carpeta.fecha.inicio", "carpeta.fecha.fin", "carpeta.todos", "carpeta.buscar"};
 
         for (String t : traduccions) {
             map.put(t.replace('.', '_'), getTraduccio(t, locale));
@@ -344,20 +436,21 @@ public class Regweb32CarpetaFrontPlugin extends RegwebDetallComponent {
         String urlDetalle = absolutePluginRequestPath + "/" + DETALL_REGISTRE_PAGE + "?numeroRegistroFormateado=";
 
         map.put("urlDetalle", urlDetalle);
-
         map.put("registros", registres);
 
         // Calculam la paginació
+        
         Paginacio paginacio = new Paginacio(totalResults, pageNumber);
-        String firstUrl = absolutePluginRequestPath + "/" + LLISTAT_REGISTRES_PAGE + "?pageNumber=1";
+        String firstUrl = absolutePluginRequestPath + "/" + LLISTAT_REGISTRES_PAGE + "?pageNumber=1" + parametros;
         String lastUrl = absolutePluginRequestPath + "/" + LLISTAT_REGISTRES_PAGE + "?pageNumber="
-                + paginacio.getTotalPages();
+                + paginacio.getTotalPages() + parametros;
+        int prevUrlIndex = (paginacio.getCurrentIndex() > 1) ? (paginacio.getCurrentIndex() - 1) : 1;
         String prevUrl = absolutePluginRequestPath + "/" + LLISTAT_REGISTRES_PAGE + "?pageNumber="
-                + (paginacio.getCurrentIndex() - 1);
+                + prevUrlIndex + parametros;
         String nextUrl = absolutePluginRequestPath + "/" + LLISTAT_REGISTRES_PAGE + "?pageNumber="
-                + (paginacio.getCurrentIndex() + 1);
-        String pageUrl = absolutePluginRequestPath + "/" + LLISTAT_REGISTRES_PAGE + "?pageNumber=";
-
+                + (paginacio.getCurrentIndex() + 2) + parametros;
+        String pageUrl = absolutePluginRequestPath + "/" + LLISTAT_REGISTRES_PAGE + "?"+parametros+"&pageNumber=";
+        
         map.put("firstUrl", firstUrl);
         map.put("lastUrl", lastUrl);
         map.put("prevUrl", prevUrl);
@@ -365,7 +458,7 @@ public class Regweb32CarpetaFrontPlugin extends RegwebDetallComponent {
         map.put("pageUrl", pageUrl);
 
         map.put("paginacio", paginacio);
-
+        
         String generat = TemplateEngine.processExpressionLanguage(plantilla, map, locale);
 
 
@@ -373,13 +466,51 @@ public class Regweb32CarpetaFrontPlugin extends RegwebDetallComponent {
 
     }
 
-    public ResultadoBusquedaWs getRegistres(String administrationID, String entidad, int pageNumber, Locale locale)
+    public ResultadoBusquedaWs getRegistres(String administrationID, String entidad, String formNumero, 
+    		Date formDataInici, Date formDataFi, String formEstat, int pageNumber, Locale locale)
             throws Exception {
 
         RegWebAsientoRegistralWs service = super.getRegWebAsientoRegistralWsService();
 
+        List<Integer> formEstats = new ArrayList<Integer>();
+        if (formEstat != null) {
+        	switch(formEstat) {
+	    		case "1":
+	    			formEstats.add(1);
+	    			formEstats.add(3);
+	    			formEstats.add(5);
+	    			formEstats.add(6);
+	    			formEstats.add(7);
+	    			formEstats.add(12);
+	    			break;
+	    		case "2":
+	    			formEstats.add(4);
+	    			formEstats.add(13);
+	    			break;
+	    		case "3":
+	    			formEstats.add(10);
+	    			break;
+	    		case "4":
+	    			formEstats.add(11);
+	    			break;
+	    	}
+        }
+            
+        if (isDevelopment()) {
+        	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	        log.info("================== PARAMETROS LLAMADA API REGWEB ===============");
+	        log.info("locale => " + locale.getLanguage());
+	        log.info("pageNumber => " + pageNumber);
+	        log.info("administrationID => " + administrationID);
+	        log.info("entidad => " + entidad);
+	        log.info("formNumero => " + formNumero);
+	        log.info("formDataInici => " + dateFormat.format(formDataInici));
+	        log.info("formDataFi => " + dateFormat.format(formDataFi));
+	        log.info("formEstats.size => " + formEstats.size());
+        }
+        
         ResultadoBusquedaWs result = service.obtenerAsientosCiudadanoCarpeta(entidad, administrationID, pageNumber,
-                locale.getLanguage());
+                locale.getLanguage(), new Timestamp(formDataInici.getTime()), new Timestamp(formDataFi.getTime()), formNumero, formEstats);
 
         if (isDevelopment()) {
             @SuppressWarnings("unchecked")
@@ -397,6 +528,7 @@ public class Regweb32CarpetaFrontPlugin extends RegwebDetallComponent {
                     log.info("ar.getResumen() => " + ar.getExtracto());
                     log.info("ar.getFechaRegistro(); => " + ar.getFechaRegistro());
                     log.info("ar.getUnidadTramitacionDestinoDenominacion() => " + ar.getDenominacionDestino());
+                    log.info("ar.getEstado() => " + ar.getEstado());
 
                     x++;
 
