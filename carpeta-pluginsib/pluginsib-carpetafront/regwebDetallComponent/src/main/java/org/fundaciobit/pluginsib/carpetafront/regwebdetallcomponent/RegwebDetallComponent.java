72985e04b8f28b4f9b942604579af8d65134f69e
Antoni Nadal <anadal@fundaciobit.org>
2021-09-27 13:59:07 +0200

    Si la URL al detall de registre no està correctament codificada, llançar error #591

--- carpeta-pluginsib/pluginsib-carpetafront/regwebDetallComponent/src/main/java/org/fundaciobit/pluginsib/carpetafront/regwebdetallcomponent/RegwebDetallComponent.java
+++ carpeta-pluginsib/pluginsib-carpetafront/regwebDetallComponent/src/main/java/org/fundaciobit/pluginsib/carpetafront/regwebdetallcomponent/RegwebDetallComponent.java
@@ -195,8 +195,15 @@ public abstract class RegwebDetallComponent extends AbstractCarpetaFrontPlugin {
             String numeroRegistroFormateado = checkRegWebNumberFormat(request.getParameter("numeroRegistroFormateado"));
 
             if (codificat) {
-                numeroRegistroFormateado = new String(Base64.getUrlDecoder().decode(numeroRegistroFormateado));
-                
+                try {
+                  numeroRegistroFormateado = new String(Base64.getUrlDecoder().decode(numeroRegistroFormateado));
+                } catch(IllegalArgumentException iae) {
+                    // "El numero de Registre (" +numeroRegistroFormateado 
+                    // ") no està codificat correctament o el parametre de l'adreça web no és complet: " + iae.getMessage()
+                    String msg = getTraduccio("registro.parametro.errorformato", locale, numeroRegistroFormateado, iae.getMessage());
+                    errorPage(msg, iae, request, response, absolutePluginRequestPath, locale);
+                    return;
+                }
             }
             
             
