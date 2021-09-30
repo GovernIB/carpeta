60d4119a9ae3fa0feec2bc62ea6eed7ee73d1348
Antoni Nadal <anadal@fundaciobit.org>
2021-09-30 10:24:41 +0200

    IOS Notificacions

--- carpeta-front/reactnative/CarpetaExpoApp/App.js
+++ carpeta-front/reactnative/CarpetaExpoApp/App.js
@@ -70,6 +70,7 @@ class App extends Component {
 
   componentDidMount() {
     registerForPushNotificationsAsync();
+
     Notifications.addNotificationReceivedListener(this._handleNotification);
 
     Notifications.addNotificationResponseReceivedListener(
