c74d14b566abaa52a1a613e67575f1e877efcb0b
jpernia-fundaciobit <jpernia@fundaciobit.org>
2022-06-16 09:34:03 +0200

    Afegir filtre de dates al plugin de NOTIB (front) #649

--- carpeta-pluginsib/pluginsib-carpetafront/notib/src/test/java/org/fundaciobit/pluginsib/carpetafront/notib/test/NotibTesterFromApi.java
+++ carpeta-pluginsib/pluginsib-carpetafront/notib/src/test/java/org/fundaciobit/pluginsib/carpetafront/notib/test/NotibTesterFromApi.java
@@ -1,5 +1,8 @@
 package org.fundaciobit.pluginsib.carpetafront.notib.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.fundaciobit.pluginsib.carpetafront.notib.api.NotibClientRest;
@@ -10,19 +13,26 @@ public class NotibTesterFromApi {
 
     public static void main(String[] args) {
 
-        String endPoint = "https://proves.caib.es/notib";
-        //String endPoint = "https://dev.caib.es/notib";
+        //String endPoint = "https://proves.caib.es/notib";
+        String endPoint = "https://dev.caib.es/notib";
         // String endPoint = "https://localhost:9999/notib";
         String usuari = "$carpeta_notib";
         String password = "carpeta_notib";
 
         NotibClientRest api = new NotibClientRest(endPoint, usuari, password);
 
-        String nif = "43096845C";
+        String nif = "XXXXXX";
         Integer pagina = 1;
         Integer mida = 20;
 
-        Resposta resposta = api.consultaNotificacions(nif, pagina, mida);
+        /* Filtre dates */
+        String formDataInici = "13%2F01%2F2022";
+        String formDataFi = "13%2F06%2F2022";
+
+        // Notificacions
+        Resposta resposta = api.consultaNotificacions(nif, formDataInici, formDataFi, pagina, mida);
+        // Comunicacions
+        // Resposta resposta = api.consultaComunicacions(nif, formDataInici, formDataFi, pagina, mida);
 
         System.out.println(" ------------  OK " + resposta + "---------------");
 
