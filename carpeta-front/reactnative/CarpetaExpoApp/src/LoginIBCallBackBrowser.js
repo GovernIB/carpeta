b609d533fe3d6b61f091465ee8db04504bb9abc0
Antoni Nadal <anadal@fundaciobit.org>
2022-08-19 13:56:03 +0200

    Pàgines Web obertes en IOS s'obren dins CarpetaAPP i no dins del navegador del mòbil #693

--- carpeta-front/reactnative/CarpetaExpoApp/src/LoginIBCallBackBrowser.js
+++ carpeta-front/reactnative/CarpetaExpoApp/src/LoginIBCallBackBrowser.js
@@ -7,6 +7,7 @@
  */
 import React from 'react';
 import {StyleSheet, ScrollView, Text} from 'react-native';
+import * as Linking from "expo-linking";
 import VistaWebComponent from './components/VistaWebComponent';
 import Persistencia from './Persistencia';
 import { sessionStorageRN } from "./SessionStorageClass";
