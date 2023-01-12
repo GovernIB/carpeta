686bc6afff4f60f2dd0d776204e25834b0e075a7
Antoni Nadal <anadal@fundaciobit.org>
2023-01-12 14:09:01 +0100

    Optimitzar generació de bundles per TypeScript #735

--- carpeta-pluginsib/pluginsib-carpetafront/apodera/react/compil.bat
+++ carpeta-pluginsib/pluginsib-carpetafront/apodera/react/compil.bat
@@ -3,7 +3,7 @@ set STARTTIME=%TIME%
 
 call npm run build
 
-xcopy  /Y .\dist\reactjs_main.js ..\src\main\resources\webpage
+xcopy  /Y .\dist\reactjs_main.js ..\src\main\resources\webpage\apodera_reactjs_main.js
 
 @echo off
 call ..\..\elapsedtime.bat %STARTTIME% %TIME%
