1096ac61d3e3d615586b765d7b58dcd6dcd8c3ec
fbosch-fundaciobit <fbosch@fundaciobit.org>
2023-05-10 13:11:10 +0200

    Crear controlador public que retorni la versió de l'aplicació #773

--- carpeta-commons/src/main/java/es/caib/carpeta/commons/utils/StaticVersion.java
+++ carpeta-commons/src/main/java/es/caib/carpeta/commons/utils/StaticVersion.java
@@ -1,15 +1,11 @@
 package es.caib.carpeta.commons.utils;
-
-
 /**
 * Codi autogenerat a partir del fitxer StaticVersion.java.template.
 */
 public interface StaticVersion {
-	
-	public static Version version = new Version();
 
-    public static final String VERSION= version.getVersion();
+    public static final String VERSION="1.1.22";
 
-    public static final String PROJECT_NAME= version.getProjectName();
+    public static final String PROJECT_NAME="carpeta";
 
 }
