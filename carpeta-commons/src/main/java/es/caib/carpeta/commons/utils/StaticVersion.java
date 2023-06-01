4bb89234eb660b3c333324d0b16fade6f7d318be
fbosch-fundaciobit <fbosch@fundaciobit.org>
2023-06-01 16:49:07 +0200

    Obrir Versió #78

--- carpeta-commons/src/main/java/es/caib/carpeta/commons/utils/StaticVersion.java
+++ carpeta-commons/src/main/java/es/caib/carpeta/commons/utils/StaticVersion.java
@@ -4,7 +4,7 @@ package es.caib.carpeta.commons.utils;
 */
 public interface StaticVersion {
 
-    public static final String VERSION="1.1.23";
+    public static final String VERSION="1.1.24";
 
     public static final String PROJECT_NAME="carpeta";
 
