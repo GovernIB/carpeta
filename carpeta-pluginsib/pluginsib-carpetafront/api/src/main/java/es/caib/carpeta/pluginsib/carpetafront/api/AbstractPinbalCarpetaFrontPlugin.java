af81100436d7b8dc3d51e303186f76f022899a4b
Antoni Nadal <anadal@fundaciobit.org>
2023-08-03 09:46:18 +0200

    No es llancen les consultes als plugins de PINBAL #813

--- carpeta-pluginsib/pluginsib-carpetafront/api/src/main/java/es/caib/carpeta/pluginsib/carpetafront/api/AbstractPinbalCarpetaFrontPlugin.java
+++ carpeta-pluginsib/pluginsib-carpetafront/api/src/main/java/es/caib/carpeta/pluginsib/carpetafront/api/AbstractPinbalCarpetaFrontPlugin.java
@@ -72,7 +72,7 @@ public abstract class AbstractPinbalCarpetaFrontPlugin extends AbstractCarpetaFr
 		
 		final String codigoCertificado = getPropertyRequired(propertyBase + "codicertificat"); 
 		
-		ClientGeneric clientRest = new ClientGeneric(baseUrl,userName,password);
+		ClientGeneric clientRest = new ClientGeneric(baseUrl,userName,password); //, true, null, null       );
 		log.info("solicitud tamany: " + solicituds.size());
 		log.info("solicitud3: " + solicituds.get(0).getDatosEspecificos());
 		resposta = clientRest.peticionSincrona(codigoCertificado, solicituds);
